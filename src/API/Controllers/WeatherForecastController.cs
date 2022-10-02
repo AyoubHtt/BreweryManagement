fa7f8933ccd40c22e69b07caad71f61242a424cd
Ayoub Httater <ayoub.httater@terabyte-software.com>
2022-10-02 15:29:45 +0100

    feat: base structure

--- src/API/Controllers/WeatherForecastController.cs
+++ src/API/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace BreweryManagement.Controllers
+namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- src/API/WeatherForecast.cs
+++ src/API/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace BreweryManagement
+namespace API
 {
     public class WeatherForecast
     {
